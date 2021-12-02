fd70259115896fca0d705f50b6f1330be2174db5
Naieem Ahmed <83205993+RNaieemAhmed@users.noreply.github.com>
2021-12-02 06:33:07 +0530

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module pro {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires javafx.base;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
